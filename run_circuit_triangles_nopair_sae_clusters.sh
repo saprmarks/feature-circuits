05cddc1b4daa8482f4d3186736b7600ea1c34219
Can Rager <can.rager@posteo.de>
2024-03-20 18:05:11 -0400

    edited cluster circuit runfile

--- run_circuit_triangles_nopair_sae_clusters.sh
+++ run_circuit_triangles_nopair_sae_clusters.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-NODE=$1
-EDGE=$2
-AGG=$3
+# NODE=$1
+# EDGE=$2
+# AGG=$3
 
 python circuit_clusters.py \
-    --cluster_param_string ERIC-QUANTA-CLUSTERS-ACTIVATIONS \
+    --cluster_param_string lin_effects_final-5-pos_nsamples8192_nctx64 \
     --clusters_path /home/can/feature_clustering/app_clusters/ \
-    --n_total_clusters 700 \
-    --start_at_cluster 0 \
+    --n_total_clusters 750 \
+    --start_at_cluster 552 \
     --dict_size 32768 \
     --dict_path /share/projects/dictionary_circuits/autoencoders/pythia-70m-deduped/ \
     --dict_id 10 \
     --device cuda:0 \
     --model_name EleutherAI/pythia-70m-deduped \
-    --batch_size 8 \
-    --node_threshold $NODE \
-    --edge_threshold $EDGE \
-    --aggregation $AGG \
-    --samples_path /home/can/feature_clustering/app_contexts/ERIC-QUANTA-CONTEXTS.json \
+    --batch_size 4 \
+    --node_threshold 0.01 \
+    --edge_threshold 0.1 \
+    --aggregation sum \
+    --samples_path /home/can/feature_clustering/app_contexts/samples8192.json \
