48ba4aaac0cfa2bcb9dba3b1973c40ce6b7a3f9f
Can Rager <can.rager@posteo.de>
2024-03-19 20:18:40 -0400

    updated runfile

--- run_circuit_triangles_nopair_sae_clusters.sh
+++ run_circuit_triangles_nopair_sae_clusters.sh
@@ -5,10 +5,10 @@ EDGE=$2
 AGG=$3
 
 python circuit_clusters.py \
-    --cluster_param_string lin_effects_final-5-pos_nsamples8192_nctx64 \
+    --cluster_param_string ERIC-QUANTA-CLUSTERS-ACTIVATIONS \
     --clusters_path /home/can/feature_clustering/app_clusters/ \
-    --n_total_clusters 750 \
-    --start_at_cluster 100 \
+    --n_total_clusters 700 \
+    --start_at_cluster 0 \
     --dict_size 32768 \
     --dict_path /share/projects/dictionary_circuits/autoencoders/pythia-70m-deduped/ \
     --dict_id 10 \
@@ -18,4 +18,4 @@ python circuit_clusters.py \
     --node_threshold $NODE \
     --edge_threshold $EDGE \
     --aggregation $AGG \
-    --samples_path /home/can/feature_clustering/clustering_pythia-70m-deduped_tloss0.1_nsamples8192_npos64_filtered-induction_attn-mlp-resid/samples8192.json \
+    --samples_path /home/can/feature_clustering/app_contexts/ERIC-QUANTA-CONTEXTS.json \
